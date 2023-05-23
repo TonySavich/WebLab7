dd132d786ac09b7816a06a82cac858fd0cf93ba8
Anton <assavich@stud.kantiana.ru>
2023-05-23 17:17:54 +0200

    .

---
+++ app/var/cache/dev/twig/44/4498eb94748860df32fc2cd10fbd301dac450588945332885fc332dee1147716.php
@@ -0,0 +1,72 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* @EasyAdmin/inspector/icon-v3.svg.twig */
+class __TwigTemplate_a52efdba7d351ef336fbed5ce33a0496bc00c772e7c3a79a494cfad5fd9dbeeb extends Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02 = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->enter($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "@EasyAdmin/inspector/icon-v3.svg.twig"));
+
+        // line 1
+        echo "<svg xmlns=\"http://www.w3.org/2000/svg\" data-icon-name=\"icon-tabler-table\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" stroke-width=\"2\" stroke=\"currentColor\" fill=\"none\" stroke-linecap=\"round\" stroke-linejoin=\"round\">
+    <path stroke=\"none\" d=\"M0 0h24v24H0z\" fill=\"none\"/>
+    <rect x=\"4\" y=\"4\" width=\"16\" height=\"16\" rx=\"2\" />
+    <line x1=\"4\" y1=\"10\" x2=\"20\" y2=\"10\" />
+    <line x1=\"10\" y1=\"4\" x2=\"10\" y2=\"20\" />
+</svg>
+";
+        
+        $__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02->leave($__internal_319393461309892924ff6e74d6d6e64287df64b63545b994e100d4ab223aed02_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "@EasyAdmin/inspector/icon-v3.svg.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  40 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("<svg xmlns=\"http://www.w3.org/2000/svg\" data-icon-name=\"icon-tabler-table\" width=\"24\" height=\"24\" viewBox=\"0 0 24 24\" stroke-width=\"2\" stroke=\"currentColor\" fill=\"none\" stroke-linecap=\"round\" stroke-linejoin=\"round\">
+    <path stroke=\"none\" d=\"M0 0h24v24H0z\" fill=\"none\"/>
+    <rect x=\"4\" y=\"4\" width=\"16\" height=\"16\" rx=\"2\" />
+    <line x1=\"4\" y1=\"10\" x2=\"20\" y2=\"10\" />
+    <line x1=\"10\" y1=\"4\" x2=\"10\" y2=\"20\" />
+</svg>
+", "@EasyAdmin/inspector/icon-v3.svg.twig", "/var/www/symfony_docker/vendor/easycorp/easyadmin-bundle/src/Resources/views/inspector/icon-v3.svg.twig");
+    }
+}
